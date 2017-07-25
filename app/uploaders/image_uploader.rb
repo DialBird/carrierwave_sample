f1b00cb3fe009d0f9c6a2b65560f2e1032985686
Keisuke Taniguchi <rememberthatk@icloud.com>
2017-07-25 05:21:55 +0000

    rails g uploader imageを実行して修正

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,20 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::RMagick
+
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  process resize_to_fill: [200, 200]
+  process convert: 'jpg'
+
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+
+  def filename
+    super.chomp(File.extname(super)) + ".jpg" if original_filename
+  end
+end
