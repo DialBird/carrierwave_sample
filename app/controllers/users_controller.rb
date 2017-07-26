2c5afbd7df635c26ef9f74407b79bd043ece5ac6
Keisuke Taniguchi <rememberthatk@icloud.com>
2017-07-26 08:48:37 +0000

    コントローラーにconfirmを追加する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,11 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def confirm
+    @user = User.new(user_params)
+    render :new if @user.invalid?
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -19,6 +24,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :age, :image)
+    params.require(:user).permit(:name, :age, :image, :image_cache)
   end
 end
