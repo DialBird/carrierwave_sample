e5ef2569477d9b30352be7b44d04ee649555ac93
Keisuke Taniguchi <rememberthatk@icloud.com>
2017-07-26 09:07:10 +0000

    initializeの修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,21 +1,22 @@
 CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: ENV['AWS_REGION'],
+    path_style: true
+  }
+  config.fog_public = true
+  config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
+  config.remove_previously_stored_files_after_update = false
+  config.fog_directory = ENV['AWS_S3_BUCKET']
+  config.asset_host = ENV['AWS_S3_URL']
+  config.cache_dir = 'tmp/image-cache'
+
   if Rails.env.development?
     config.cache_storage = :file
   else
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: ENV['AWS_REGION'],
-      path_style: true
-    }
     config.cache_storage = :fog
-    config.cache_dir = 'tmp/image-cache'
-    config.fog_public = true
-    config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
-    config.remove_previously_stored_files_after_update = false
-    config.fog_directory = ENV['AWS_S3_BUCKET']
-    config.asset_host = ENV['AWS_S3_URL']
   end
 end
 
